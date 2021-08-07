905a35c507e3e015aeaa20108b40ba2007f34d13
Dhia Djobbi <badboy.dhia@gmail.com>
2021-08-07 13:53:56 +0100

    firebase crud + datatable

--- config/bundles.php
+++ config/bundles.php
@@ -4,4 +4,7 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Kreait\Firebase\Symfony\Bundle\FirebaseBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Service/UserService.php
+++ src/Service/UserService.php
@@ -15,15 +15,50 @@ class UserService
         $this->database = $database;
     }
 
+    public function findAll(): array
+    {
+        return $this->database->getReference('User')->getValue();
+    }
+
+
+    public function new($user)
+    {
+        $newKey = $this->database->getReference('User')->push()->getKey();
+        $this->database->getReference('User/' . $newKey . "/")->set($user);
+    }
+
+    public function edit($key,$newuser)
+    {
+        $this->database->getReference('User/' . $key . "/")->set($newuser);
+    }
+
+
     public function findByEmail($username): array
     {
         $userTable = $this->database->getReference('User');
-        $user=$userTable
-        ->orderByChild("email")
-        ->equalTo($username)
-        ->getSnapshot()
-        ->getValue();
+        $user = $userTable
+            ->orderByChild("email")
+            ->limitToFirst(1)
+            ->equalTo($username)
+            ->getSnapshot()
+            ->getValue();
         return $user;
     }
 
+    public function findByEmailLike($username)
+    {
+        $userTable = $this->database->getReference('User');
+        $user = $userTable
+            ->orderByChild("email")
+            ->startAt($username)  
+            ->endAt($username."\uf8ff")
+            ->getSnapshot()
+            ->getValue();
+        return $user;
+    }
+
+    public function remove($key)
+    {
+        $this->database->getReference('User/' . $key)->remove();
+    }
 }
